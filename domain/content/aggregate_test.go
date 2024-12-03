97ed2895dd3bb24a9307c96af212be5ce468584d
amansman77 <amansman77@gmail.com>
2024-12-03 13:18:32 +0900

    테스트 케이스 추가 : 필드에 두개의 댓글을 추가한다 (#3)
    테스트 케이스 추가 : 알 수 없는 이벤트 타입이면 ErrUnknownEventType을 반환한다
    
    Co-authored-by: hshwang <hshwang@fxgear.net>

--- domain/content/aggregate_test.go
+++ domain/content/aggregate_test.go
@@ -143,4 +143,36 @@ func TestContentAggregate_AddFieldComment(t *testing.T) {
 		assert.Equal(t, "testerId", actualFieldComment.Comments[0].CreatedById)
 		assert.Equal(t, "testerName", actualFieldComment.Comments[0].CreatedByName)
 	})
+
+	t.Run("필드에 두개의 댓글을 추가한다", func(t *testing.T) {
+		// given
+		sut, _ := NewContentAggregate(uuid.New().String(), "bettercode")
+		sut.Content = map[string]any{"name": "홍길동"}
+		_ = sut.AddFieldComment(context.Background(), "name", "첫번째 댓글", "user1", "사용자1")
+
+		// when
+		err := sut.AddFieldComment(context.Background(), "name", "두번째 댓글", "user2", "사용자2")
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(sut.FieldComments))
+		actualFieldComment := sut.FieldComments[0]
+		assert.Equal(t, 2, len(actualFieldComment.Comments))
+		assert.Equal(t, "두번째 댓글", actualFieldComment.Comments[1].Comment)
+		assert.Equal(t, "user2", actualFieldComment.Comments[1].CreatedById)
+	})
+}
+
+func TestContentAggregate_When(t *testing.T) {
+	t.Run("알 수 없는 이벤트 타입이면 ErrUnknownEventType을 반환한다", func(t *testing.T) {
+		// given
+		sut, _ := NewContentAggregate(uuid.New().String(), "bettercode")
+		unknownEvent := struct{}{}
+
+		// when
+		err := sut.When(unknownEvent)
+
+		// then
+		assert.ErrorIs(t, err, ErrUnknownEventType)
+	})
 }
